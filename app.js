8b14828e40e1826d2ebd41edab30e8f9d6fc61ca
mabielmuller <mabielmuller9@gmail.com>
2023-09-24 21:55:23 -0300

    criando o css da pagina

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const exphbs = require('express-handlebars');
 const app = express()
+const path = require('path')
 const db = require('./db/connection')
 const bodyParser = require ('body-parser')
+
 
 const PORT = 3000
 
@@ -11,6 +14,16 @@ app.listen(PORT, function(){
 
 //body parser
 app.use(bodyParser.urlencoded({ extended: false }))
+
+
+//Handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 // db connection
 db
@@ -25,7 +38,7 @@ db
 
 // routes
 app.get('/', (req, res) => {
-    res.send("Está funcionando corretamente")
+    res.render('index')
 })  
 
 // jobs routes
